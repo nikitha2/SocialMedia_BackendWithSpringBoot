b6159dbfafe72e93068da4736efbbca6127106f1
nikitha2 <gullapalli.nikitha@gmail.com>
2023-04-16 16:38:49 -0700

    Beans

---
+++ src/main/java/com/springboot/social_media/user/User.java
@@ -0,0 +1,45 @@
+package com.springboot.social_media.user;
+
+public class User {
+	
+	private Integer id;
+	private String name;
+	private String birthDate;
+	
+	public User(Integer id, String name, String birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
